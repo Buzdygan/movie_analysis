3a83743bb50f9ecd38c6d3cd4e72ff16d12256b0
jakub <tlalka.jakub@gmail.com>
2019-02-18 17:18:15 +0100

    Clean scrape awards notebook

--- src/prediction.py
+++ src/prediction.py
@@ -1,25 +1,29 @@
+import warnings
+import sys
 from itertools import product
 
 import pandas as pd
 import numpy as np
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.linear_model import LogisticRegression, LinearRegression, SGDClassifier
 from sklearn.svm import SVC
 
-
+from utils import get_movie_df
 from constants import AWARD_COLUMNS
 
+# AWARDS + CRITICS >= 2000 : Avg Nscore: 0.84, Rank Score: 0.91, Accuracy: 0.63
 
 BASE_COLUMNS = ['year', 'title', 'winner']
 PRED_SCORE_COL = 'predicted_score'
 AWARDS = 'Awards'
 CRITICS = 'Critics'
-EVALUATION_START_YEAR = 2000
+EVALUATION_START_YEAR = 1960
 
 def iterate_cases(full_df, level=None):
     for year in full_df['year'].drop_duplicates().tolist():
         if year >= EVALUATION_START_YEAR:
             if level:
+                print(f"{level} Year: {year}", file=sys.stderr)
                 print(f"{level} Year: {year}")
             df = full_df.copy()
             year_train_df = df[df.year != year]
@@ -40,19 +44,44 @@ def get_accuracy(orig_df, col=PRED_SCORE_COL):
     return df[df.winner]['pred_winner'].mean()
 
 
-class BasePredictor(object):
-    @classmethod
-    def hparam_combinations(cls):
-        return [{}]
+def get_rank_score(orig_df, col=PRED_SCORE_COL):
+    df = orig_df.copy()
+    df['rank'] = df.groupby('year')[col].rank(ascending=False)
+    year_cnt_df = df.groupby('year')[col].count().reset_index().rename(columns={col: 'year_cnt'})
+    df = df.merge(year_cnt_df)
+    df['rscore'] = df.apply(lambda x: 1.0 - (x['rank'] - 1) / (x['year_cnt'] - 1), axis=1) 
+    return df[df.winner]['rscore'].mean()
 
-    def __init__(self, train_df, test_df, hparam_dict):
+
+def choose_hparams(predictor_cls, train_df, evaluation_method=get_rank_score):
+    best_score = 0.0
+    best_hparams = None
+    for hparams in predictor_cls.HPARAM_COMBINATIONS:
+        year_dfs = []
+        for year_train_df, year_test_df, year_df in iterate_cases(train_df):
+            predictor = predictor_cls(year_train_df, year_test_df, hparams)
+            year_df[PRED_SCORE_COL] = predictor.predict()
+            year_dfs.append(year_df)
+        result_df = pd.concat(year_dfs)
+        score = evaluation_method(result_df) 
+        if score > best_score:
+            best_score = score
+            best_hparams = hparams
+    print(f"Best hparams for {predictor_cls.__name__}: {best_hparams} : {best_score}")
+    return best_hparams, best_score
+
+
+class BasePredictor(object):
+    HPARAM_COMBINATIONS = [{}]
+
+    def __init__(self, train_df, test_df, hparams=None):
         self._train_df = self.adjust_data(train_df)
         self._test_df = self.adjust_data(test_df)
-        self._hparam_dict = hparam_dict
+        self._hparams = hparams
 
     @property
     def hparams(self):
-        return self._hparam_dict.get(self.NAME, {})
+        return self._hparams
 
     @property
     def NAME(self):
@@ -88,26 +117,33 @@ class SklearnPredictor(BasePredictor):
         model = self.MODEL_CLASS(**self.hparams).fit(*self.xy(self._train_df))
         return pd.Series(model.predict_proba(self.xy(self._test_df)[0])[:, 1])
 
+class GradientBoostingPredictor(SklearnPredictor):
+    MODEL_CLASS = GradientBoostingClassifier
+    HPARAM_COMBINATIONS = [{'loss': loss, 'learning_rate': lrate, 'max_depth': max_depth}
+                           for loss in ['deviance', 'exponential']
+                           for lrate in [0.01, 0.03, 0.1, 0.3, 1.0]
+                           for max_depth in [2, 3, 4, 5]]
+
 class RandomForestPredictor(SklearnPredictor):
     MODEL_CLASS = RandomForestClassifier
+    HPARAM_COMBINATIONS = [{'criterion': criterion, 'max_depth': max_depth, 'n_estimators': n_estimators}
+                           for criterion in ['gini', 'entropy']
+                           for max_depth in [None, 2, 3, 4]
+                           for n_estimators in [10, 50, 100]]
 
 class LogisticPredictor(SklearnPredictor):
     MODEL_CLASS = LogisticRegression
+    HPARAM_COMBINATIONS = [{'C': c} for c in [0.1, 0.3, 1.0, 3.0]]
 
 class SVCPredictor(SklearnPredictor):
     MODEL_CLASS = SVC
-
-    @classmethod
-    def hparam_combinations(cls):
-        return [{'probability': True}]
+    HPARAM_COMBINATIONS = [{'probability': True, 'C': c}
+                           for c in [0.1, 0.3, 1.0, 3.0, 10.0]]
 
 class SGDPredictor(SklearnPredictor):
     MODEL_CLASS = SGDClassifier
-
-    @classmethod
-    def hparam_combinations(cls):
-        return [{'loss': 'log', 'alpha': alpha}
-                for alpha in [0.1, 0.01, 0.001]]
+    HPARAM_COMBINATIONS = [{'loss': 'log', 'alpha': alpha}
+                           for alpha in [0.01, 0.03, 0.1, 0.3]]
 
 
 def P(predictor_cls, tp='Awards'):
@@ -122,14 +158,22 @@ class SimpleEnsemblePredictor(BasePredictor):
                   for pred_cls in [RandomForestPredictor,
                                    LogisticPredictor,
                                    SVCPredictor,
-                                   SGDPredictor
+                                   SGDPredictor,
+                                   GradientBoostingPredictor
                                   ]
                  ]
 
     def predict(self):
-        results = [predictor_cls(self._train_df, self._test_df, self._hparam_dict).predict()
-                   for predictor_cls in self.PREDICTORS]
-        return sum(results) / len(self.PREDICTORS)
+        results = []
+        weight_sum = 0.0
+        for predictor_cls in self.PREDICTORS:
+            hparams, weight = choose_hparams(predictor_cls, self._train_df)
+            predictor = predictor_cls(self._train_df,
+                                      self._test_df,
+                                      hparams)
+            results.append(predictor.predict() * weight)
+            weight_sum += weight
+        return sum(results) / weight_sum
 
 
 class EnsemblePredictor(BasePredictor):
@@ -163,14 +207,18 @@ class EnsemblePredictor(BasePredictor):
         year_dfs = []
         for year_train_df, year_test_df, year_df in iterate_cases(self._train_df):
             for predictor_cls in self.PREDICTORS:
-                predictor = predictor_cls(year_train_df, year_test_df, self._hparam_dict)
+                predictor = predictor_cls(year_train_df,
+                                          year_test_df,
+                                          choose_hparams(predictor_cls, year_train_df))
                 year_df[predictor.NAME] = predictor.predict()
             year_dfs.append(year_df)
         train_df = pd.concat(year_dfs)
 
         test_df = self._test_df.copy()[['year', 'title', 'winner']].reset_index()
         for predictor_cls in self.PREDICTORS:
-            predictor = predictor_cls(self._train_df, self._test_df, self._hparam_dict)
+            predictor = predictor_cls(self._train_df,
+                                      self._test_df,
+                                      choose_hparams(predictor_cls, self._train_df))
             test_df[predictor.NAME] = predictor.predict()
 
         # Normalize
@@ -189,44 +237,24 @@ class EnsemblePredictor(BasePredictor):
         return train_df, test_df
 
 
-class WeightAdjustingPredictor(BasePredictor):
-    ENSEMBLE_CLASS = SimpleEnsemblePredictor
-    def _possible_hparams(self):
-        predictors = self.ENSEMBLE_CLASS.PREDICTORS + [self.ENSEMBLE_CLASS]
-        for choice in product(*[pred.hparam_combinations() for pred in predictors]):
-            yield {predictor.__name__: hdict for predictor, hdict in zip(predictors, choice)}
-
-    def predict(self):
-        best_score, best_hparams = 0, None
-        for hparams_dict in self._possible_hparams():
-            print("Try hparams")
-            print(hparams_dict)
-            year_dfs = []
-            for year_train_df, year_test_df, year_df in iterate_cases(self._train_df):
-                ens_predictor = self.ENSEMBLE_CLASS(year_train_df, year_test_df, hparams_dict)
-                year_df[PRED_SCORE_COL] = ens_predictor.predict()
-                year_dfs.append(year_df)
-            predicted_df = pd.concat(year_dfs)
-            score = get_avg_norm_score(predicted_df)
-            if score > best_score:
-                best_score = score
-                best_hparams = hparams_dict
-            print(f"Score: {score}, best: {best_score}")
-        
-        ens_predictor = self.ENSEMBLE_CLASS(self._train_df, self._test_df, best_hparams)
-        return ens_predictor.predict()
-
-
 def evaluate(data_df):
     year_dfs = []
     for ytrain_df, ytest_df, ydf in iterate_cases(data_df, "Outer Evaluation"):
-        wa_predictor = WeightAdjustingPredictor(ytrain_df, ytest_df, {})
+        wa_predictor = SimpleEnsemblePredictor(ytrain_df, ytest_df)
         ydf[PRED_SCORE_COL] = wa_predictor.predict()
         year_dfs.append(ydf)
+        print(f"Year rscore: {get_rank_score(ydf):.2}")
     result_df = pd.concat(year_dfs)
+    avg_rscore = get_rank_score(result_df)
     avg_nscore = get_avg_norm_score(result_df)
     accuracy = get_accuracy(result_df)
-    print(f"Avg Nscore: {avg_nscore:.2}, Accuracy: {accuracy:.2}")
+    print(f"Avg Nscore: {avg_nscore:.2}, Rank Score: {avg_rscore:.2}, Accuracy: {accuracy:.2}")
+    return result_df
 
 
 
+if __name__ == "__main__":
+    warnings.filterwarnings('ignore')
+    movie_df = get_movie_df()
+    result_df = evaluate(movie_df)
+    result_df.to_csv('results_awards_1960.csv')
